2ed874fe906439a6c092b4dfc1f86833612a34a5
Jamie Turner <jamie@bu.mp>
2013-04-12 02:45:21 -0700

    Be better about including zlib license.

--- springfield.c
+++ springfield.c
@@ -362,14 +362,35 @@ static uint32_t jenkins_one_at_a_time_hash(char *key, size_t len)
     return hash;
 }
 
-/* -- (modified) from zlib -- */
+/* -- CRC32 courtesy of zlib -- */
 
-/* crc32.c -- compute the CRC-32 of a data stream
- * Copyright (C) 1995-1998 Mark Adler
- * For conditions of distribution and use, see copyright notice in zlib.h 
- */
+/* Note: modified by springfield project for style and
+   to remove dependencies on zlib.h 
+*/
 
-/* @(#) $Id: crc32.c,v 1.1.1.1 2000/10/19 05:38:50 root Exp $ */
+/*
+  Copyright (C) 1995-2010 Jean-loup Gailly and Mark Adler
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Jean-loup Gailly        Mark Adler
+  jloup@gzip.org          madler@alumni.caltech.edu
+
+*/
 
 /* ========================================================================
  * Table of CRC-32's of all single-byte values (made by make_crc_table)
